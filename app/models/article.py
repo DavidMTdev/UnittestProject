78ea780e7dc0f5adf524f4006eb8de67ad63fde6
DavidMTdev <david.teixeira31@gmail.com>
2022-02-24 11:58:20 +0100

    Update _id: str ->_id: ObjectId in mongodb

--- app/models/article.py
+++ app/models/article.py
@@ -12,6 +12,7 @@ class Article(BaseModel):
     memoryType: str = Field(default=None)
 
     class Config:
+        # allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {
             ObjectId: str
